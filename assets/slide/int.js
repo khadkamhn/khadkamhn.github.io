1befc7faf9e2dae4736d5210c949365ad7e919fb
Mohan Khadka <khadkamhn@gmail.com>
2014-05-09 14:10:48 +0545

    mohan
    mohan

--- assets/slide/int.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){var options={nextButton:true,prevButton:true,pagination:true,animateStartingFrameIn:true,autoPlay:false,autoPlayDelay:3E3,preloader:true};var mySequence=$("#sequence").sequence(options).data("sequence")});
