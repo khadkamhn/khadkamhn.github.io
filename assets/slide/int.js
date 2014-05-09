8dbf09eb093c4c6126f792d442ae89757b7c5667
Mohan Khadka <khadkamhn@gmail.com>
2014-05-09 14:03:32 +0545

    Mohan Khadka
    Personal Page

---
+++ assets/slide/int.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){var options={nextButton:true,prevButton:true,pagination:true,animateStartingFrameIn:true,autoPlay:false,autoPlayDelay:3E3,preloader:true};var mySequence=$("#sequence").sequence(options).data("sequence")});
